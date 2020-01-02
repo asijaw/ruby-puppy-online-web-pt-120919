80925079d2b357eaa9a3fd727d55c2111f30eb9c
Asija Watson <asija.w@yahoo.com>
2020-01-02 02:53:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save(name)
+  end 
+  
+  def save(name)
+    @all << name
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @all.each {|dog| puts dog }
+  end 
+end
+  
